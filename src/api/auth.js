7ed1f805734e59ffe9599d4ede2105bebbbe4211
cluster_wraith <69491293+martins0023@users.noreply.github.com>
2024-08-18 09:40:24 -0700

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api/auth" });
+const API = axios.create({ baseURL: "https://ecommerce-backend-n793.onrender.com/api/auth" });
 
 export const login = (formData) => API.post("/login", formData);
 export const signup = (formData) => API.post("/signup", formData);
